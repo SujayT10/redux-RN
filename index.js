426798984e37048ad9ac329815f82fb4d6e8f2e5
sujayta <mh27bh3242>
2023-06-29 12:54:08 +0530

    roorReducer and store part added

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json'
+import { Provider } from 'react-redux';
+import store from './components/redux/store';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppRedux = () => (
+    <Provider store={store}>
+        <App></App>
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => AppRedux);
+
